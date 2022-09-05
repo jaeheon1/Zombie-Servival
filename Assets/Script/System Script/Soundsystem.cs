a1978f4d34e75a2e5b967a08af35adf257531764
jaeheon1 <jungjaehen@naver.com>
2022-09-05 19:46:54 +0900

    zombie

--- Assets/Script/System Script/Soundsystem.cs
+++ Assets/Script/System Script/Soundsystem.cs
@@ -3,11 +3,23 @@ using UnityEngine;
 
 public class Soundsystem : MonoBehaviour
 {
-    
     AudioSource audioSource;
-    [SerializeField] AudioClip[] clip;
-
+    [SerializeField] AudioClip[] Clip;
     public static Soundsystem instance;
+    private void Awake()
+    {
+
+        //내가 찾는 오브젝트의 타입이 Sound
+        var soundObject=FindObjectsOfType<Soundsystem>();
+        if(soundObject.Length >1)
+        {
+            //자기 자신을 파괴해서 오직 하나의 SoundSystem 오브젝트만 남길수 있도록 합니다.
+            Destroy(gameObject);
+        }
+
+    }
+
+   
     void Start()
     {
         if(instance ==null)
@@ -22,7 +34,7 @@ public class Soundsystem : MonoBehaviour
 
     public void Sound(int number)
     {
-        audioSource.PlayOneShot(clip[number]);
+        audioSource.PlayOneShot(Clip[number]);
 
     }
 }
